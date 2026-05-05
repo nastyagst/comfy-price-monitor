b37bb3c7a2809b11571dd4a68cd26904c6c5559b
nastya <hociy2015@gmail.com>
2026-05-05 19:37:55 +0300

    fix

--- monitor/services.py
+++ monitor/services.py
@@ -1,7 +1,8 @@
+import os
 import httpx
 from bs4 import BeautifulSoup
 from decimal import Decimal
-from monitor.models import Product
+from monitor.models import Product, PriceAlert
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,15 +21,15 @@ def update_product_price(product_id):
             response = client.get(product.url)
 
             if response.status_code != 200 and len(response.text) < 5000:
-                print(f"Помилка: статус {response.status_code}, контент замалий")
                 return False
 
             soup = BeautifulSoup(response.text, "html.parser")
 
+            # Шукаємо ціну для Brain, Comfy або ITbox
             price_element = (
-                soup.find("span", class_="price-number")  # Brain
-                or soup.find("div", class_="price__current")  # Comfy
-                or soup.find("div", class_="price")  # ITbox
+                soup.find("span", class_="price-number")
+                or soup.find("div", class_="price__current")
+                or soup.find("div", class_="price")
             )
 
             if price_element:
@@ -37,15 +38,44 @@ def update_product_price(product_id):
 
                 if clean_price:
                     product.current_price = Decimal(clean_price)
-
                     title_element = soup.find("h1")
                     if title_element:
                         product.title = title_element.get_text(strip=True)
-
                     product.save()
                     return True
-
-            print(f"Ціну не знайдено на сторінці")
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error parsing: {e}")
     return False
+
+
+def get_user_alerts_list():
+    alerts = PriceAlert.objects.filter(is_active=True)
+    if not alerts.exists():
+        return "У тебе поки немає активних підписок."
+
+    response = "<b>Твій список моніторингу:</b>\n\n"
+    for alert in alerts:
+        response += (
+            f"📍 {alert.product.title}\n"
+            f"Ціна зараз: {alert.product.current_price} грн\n"
+            f"Цільова: {alert.target_price} грн\n"
+            f"<a href='{alert.product.url}'>Посилання</a>\n"
+            f"-------------------\n"
+        )
+    return response
+
+
+def send_telegram_notification(title, old_price, new_price, url):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("MY_CHAT_ID")
+    message = (
+        f"📉 <b>Ціна впала!</b>\n\n"
+        f"Товар: {title}\n"
+        f"Стара ціна: {old_price} грн\n"
+        f"Нова ціна: <b>{new_price} грн</b>\n\n"
+        f"<a href='{url}'>Купити</a>"
+    )
+    api_url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message, "parse_mode": "HTML"}
+    with httpx.Client() as client:
+        client.post(api_url, json=payload)
